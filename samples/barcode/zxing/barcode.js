d3f4501cb3a5944297c345db3bdb78801edf3c1e
eddieshan <devyalowda@gmail.com>
2022-07-31 19:31:59 +0200

    Cam selection

--- samples/barcode/zxing/barcode.js
+++ samples/barcode/zxing/barcode.js
@@ -1,75 +1,126 @@
-const zxing = window["zxing-wasm"];
-
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-let W = 0;
-let H = 0;
-let buf;
-
-function toGrayScale(frame) {
+function toGrayScale(frame, grayBuffer) {
   let j = 0;
 
   for (let i = 0; i < frame.length; i++) {
     if (i % 4 === 0) {
-      buf[j] = (frame[i] + frame[i + 1] + frame[i + 2]) / 3;
+      grayBuffer[j] = (frame[i] + frame[i + 1] + frame[i + 2]) / 3;
       j++;
     }
   }
   
-  return buf;
+  return grayBuffer;
 }
 
 
-let frame_count = 0;
+let frameCount = 0;
 
-function processVideo(src) {
-  ctx.drawImage(src, 0, 0, W, H);
-  const frame = ctx.getImageData(0, 0, W, H);
+function newScanner(player) {
+
+  const canvas = document.querySelector("canvas");
+
+  canvas.width = player.videoWidth;
+  canvas.height = player.videoHeight;
+
+  return {
+    zxing: window["zxing-wasm"],
+    drawArea: canvas,
+    context: canvas.getContext("2d")
+  };
+}
+
+function processVideo(src, scanner, grayBuffer) {
+  scanner.context.drawImage(src, 0, 0, scanner.drawArea.width, scanner.drawArea.height);
+  //console.log("Width: ", scanner.drawArea.width, ", height", scanner.drawArea.height);
+  const frame = scanner.context.getImageData(0, 0, scanner.drawArea.width, scanner.drawArea.height);
   const scanMessage = document.getElementById("scan-result");
+  toGrayScale(frame.data, grayBuffer);
 
-  zxing
-    .read(toGrayScale(frame.data), frame.width, frame.height)
+  scanner.zxing
+    .read(grayBuffer, frame.width, frame.height)
     .then(result => {
-
       if (result === "") {
-        frame_count = frame_count === 60? 0 : frame_count + 1;
-        scanMessage.textContent = `[Scanning] ${frame_count}`;
-        setTimeout(() => processVideo(src, false), 0);
+        frameCount = frameCount === 60? 0 : frameCount + 1;
+        scanMessage.textContent = `[Scanning] ${frameCount}`;
+        setTimeout(() => processVideo(src, scanner, grayBuffer), 0);
       } else {
         scanMessage.textContent = result;
       }
     });
 }
 
-function clearBuffer(w, h) {
-  W = w;
-  H = h;
-  canvas.width = W;
-  canvas.height = H;
-  buf = new Uint8Array(new Array(W * H).fill(0).map(_ => 0))
+
+function startCapture() {
+  console.log("Camera", document.getElementById("video-source").value);
+  const cameraId = document.getElementById("video-source").value;
+  const player = document.querySelector("video");
+
+  window.navigator.mediaDevices
+      .getUserMedia({ video: { deviceId: { exact: cameraId } } })
+      .then(stream => {
+        player.srcObject = stream;
+        player.onloadedmetadata = () => {        
+          const scanner = newScanner(player);  
+          const area = scanner.drawArea.width * scanner.drawArea.height;
+          const grayBuffer = new Uint8Array(new Array(area).fill(0).map(_ => 0));
+          setTimeout(() => processVideo(player, scanner, grayBuffer), 0);
+        };
+      })
+      .catch(e => { 
+        player.stop();
+        console.error(e.name + ": " + e.message);
+        alert(e); 
+      });
 }
 
-function init(player, stream) {
-    player.srcObject = stream;
-
-    player.onloadedmetadata = () => {
-        clearBuffer(player.videoWidth, player.videoHeight);
-        setTimeout(() => processVideo(player, false), 0)
-    };
+function getVideoDevices() {
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then(devices => { 
+      let cameras = devices.filter(device => device.kind == "videoinput");
+      listCameras(cameras);
+    })
+    .catch(function (e) {
+      console.log(e.name + ": " + e.message);
+    });
 }
 
-document
+function listCameras(cameras) {
+  let videoSourcesSelect = document.getElementById("video-source");  
+  cameras
+    .forEach(camera => {
+      let option = new Option();
+      option.value = camera.deviceId;
+      option.text = camera.label || `Camera ${videoSourcesSelect.length + 1}`;
+      videoSourcesSelect.appendChild(option);
+  });
+}
+
+// function getVideoDevices() {
+//   navigator.mediaDevices
+//     .enumerateDevices()
+//     .then(devices => {
+//       let videoSourcesSelect = document.getElementById("video-source");
+
+//       // Iterate over all the list of devices (InputDeviceInfo and MediaDeviceInfo)
+//       devices
+//         .filter(device => device.kind == "videoinput")
+//         .forEach(device => {
+//           let option = new Option();
+//           option.value = device.deviceId;
+//           option.text = device.label || `Camera ${videoSourcesSelect.length + 1}`;
+//           videoSourcesSelect.appendChild(option);
+//       });
+//     }).catch(function (e) {
+//       console.log(e.name + ": " + e.message);
+//     });
+// }
+
+document.addEventListener( "DOMContentLoaded", function(event){
+  document
     .querySelector("button")
-    .addEventListener("click", () => {
+    .addEventListener("click", startCapture);
 
-        const player = document.querySelector("video");
-        canvas.style = "transform: scale(-1, 1);";
-
-        window.navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then(mediaStream => init(player, mediaStream))
-    .catch(e => { player.stop(); alert(e); });
-})
-
-// document.addEventListener( "DOMContentLoaded", function(event){
-// }, false );
+  getVideoDevices();
+  //listCameras();
+  
+}, false );
